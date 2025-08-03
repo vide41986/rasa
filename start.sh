4e7f0637c4186871564482d454de092549303815
vide41986 <vide41986@gmail.com>
2025-08-03 10:58:10 +0530

    Updated start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ rasa run actions --port 5055 &
 sleep 5
 
 # Start Rasa server
-rasa run --model models/ --enable-api --cors "*" --port 5005
+rasa run --model models/20250803-043322-dichotomic-photon.tar.gz --enable-api --cors "*" --port 5005
